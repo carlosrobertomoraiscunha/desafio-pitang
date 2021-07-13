e57d28ec73bd8e4fe99f0bcf97f26785c112b4cf
Carlos Roberto Morais Cunha <42938356+carlosrobertomoraiscunha@users.noreply.github.com>
2021-07-13 17:57:18 -0300

    Delete desafio-pitang directory

--- desafio-pitang/src/main/java/repository/interfaces/IUsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package repository.interfaces;
-
-import models.Usuario;
-
-public interface IUsuarioRepository extends IRepository<Usuario, Integer> {
-	public Usuario pegarPorEmailESenha(String email, String senha);
-}
